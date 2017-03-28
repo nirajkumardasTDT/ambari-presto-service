8deeb950bca9e4f145f06f5acfea49b6ebb8a83a
Niraj Kumar Das <niraj.kumar.das@thedatateam.in>
2017-03-28 15:03:34 +0530

    changes

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 rm -rf /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO
-#rm -rf /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO
+rm -rf /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO
 
 mkdir /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO
 
@@ -14,8 +14,8 @@ mv /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO/ambari-presto
 rm -rf /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO/ambari-presto-1.2
 
 
-#mkdir /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO
-#cp -R /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/
+mkdir /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO
+cp -R /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/
 
 chmod -R +x /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO/*
-#chmod -R +x /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO/*
+chmod -R +x /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO/*
