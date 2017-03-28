91abca0a32507b4cb84ad8ad789e11fe2707908f
Niraj Kumar Das <niraj.kumar.das@thedatateam.in>
2017-03-28 17:50:31 +0530

    changes

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ mkdir /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO
 
 cd /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO
 
-wget https://github.com/nirajkumardasTDT/ambari-presto-service/archive/ambari-presto-1.2.2.ta1r.gz
+wget https://github.com/nirajkumardasTDT/ambari-presto-service/archive/ambari-presto-1.2.2.tar.gz
 
 tar -xvf ambari-presto-1.2.2.tar.gz -C /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO
 mv /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO/ambari-presto-service-ambari-presto-1.2.2/* /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO
