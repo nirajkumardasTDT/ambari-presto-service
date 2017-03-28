31f75b7958d4ffe74b5de7854939343f9a9c54df
Niraj Kumar Das <niraj.kumar.das@thedatateam.in>
2017-03-28 15:27:01 +0530

    changes

--- start.sh
+++ start.sh
@@ -15,7 +15,8 @@ rm -rf /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO/ambari-pr
 
 
 mkdir /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO
-
+cd /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO
+wget https://github.com/prestodb/ambari-presto-service/releases/download/v1.2/ambari-presto-1.2.tar.gz
 tar -xvf ambari-presto-1.2.tar.gz -C /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO
 mv /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO/ambari-presto-1.2/* /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO
 rm -rf /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO/ambari-presto-1.2
