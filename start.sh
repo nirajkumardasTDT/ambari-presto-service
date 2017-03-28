cff6e7d6ee9430303790f9506df0eda5334bcc0c
Niraj Kumar Das <niraj.kumar.das@thedatateam.in>
2017-03-28 16:30:50 +0530

    changes

--- start.sh
+++ start.sh
@@ -12,16 +12,16 @@ cd /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO
 wget https://github.com/nirajkumardasTDT/ambari-presto-service/archive/ambari-presto-1.2.1.tar.gz
 
 tar -xvf ambari-presto-1.2.1.tar.gz -C /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO
-mv /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO/ambari-presto-1.2.1/* /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO
-rm -rf /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO/ambari-presto-1.2.1
+mv /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO/ambari-presto-service-ambari-presto-1.2.1/* /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO
+rm -rf /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO/ambari-presto-service-ambari-presto-1.2.1
 
 
 mkdir /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO
 cd /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO
 wget https://github.com/nirajkumardasTDT/ambari-presto-service/archive/ambari-presto-1.2.1.tar.gz
 tar -xvf ambari-presto-1.2.1.tar.gz -C /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO
-mv /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO/ambari-presto-1.2.1/* /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO
-rm -rf /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO/ambari-presto-1.2.1
+mv /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO/ambari-presto-service-ambari-presto-1.2.1/* /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO
+rm -rf /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/PRESTO/ambari-presto-service-ambari-presto-1.2.1
 
 #cp -R /var/lib/ambari-server/resources/stacks/HDP/2.5/services/PRESTO /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/
 
